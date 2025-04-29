a1dc4027d1665a38481e1a83670162a67b29c69d
irfanahmed02 <irfanahmed.s@yahoo.com>
2025-04-30 02:26:27 +0300

    feat: getting a user input

--- bankaccount.py
+++ bankaccount.py
@@ -23,26 +23,51 @@ class BankAccount:
 
   def deposit(self, amount):
     if amount <= 0:
-      raise ValueError('Deposit amount must be positive')
+      return 'Deposit amount must be positive'
     self.balance += amount
     self.get_transaction_id("deposit",amount)
     return f"{amount} has been successfully  deposited into your account"
 
   def withdraw(self, amount):
     if amount <= 0:
-      raise ValueError('Withdrawal amount must be positive')
+      return 'Withdrawal amount must be positive'
     if amount > self.balance:
-      raise ValueError('Insufficient funds')
+      return 'Insufficient funds'
     self.balance -= amount
     self.get_transaction_id("withdraw",amount)
-    return f"{amount} has been successfully credit from your account"
+    return f"{amount} has been successfully withdrawn from your account"
 
   def check_balance(self):
     return f"Current Balance: {self.balance}"
   
 
-
+my_account = BankAccount()
+while True:
+  operation = int(input("Please enter the transaction number \n 1. Deposit \n 2. Withdrawl \n 3. Statement \n 4. Check balance \n 5. Exit \n"))
   
+  if operation == 1:
+    amount = int(input("Please enter the deposit amount \n"))
+    print(my_account.deposit(amount))
+
+  elif operation == 2:
+    amount = int(input("Please enter withdraw amount \n"))
+    print(my_account.withdraw(amount))
+
+  elif operation == 3:
+    transactions = my_account.get_transactions_statement()
+    print("Transaction ID, Date, Type, Amount")
+    for id, tran in transactions.items():
+      print(f"{id},{transactions[id]["date"]},{transactions[id]["transaction"]},{transactions[id]["amount"]}")
+
+  elif operation == 4:
+    print(my_account.check_balance())
+
+  elif operation == 5:
+    break
+
+  else:
+    print("Please enter valid key: \n")
+
 
 
   
