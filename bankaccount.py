2f5723e6b8fcf836107188694a4a4cfd2ae29e46
irfanahmed02 <irfanahmed.s@yahoo.com>
2025-04-30 04:58:39 +0300

    feat: added feature to load the previous transactions

--- bankaccount.py
+++ bankaccount.py
@@ -1,4 +1,5 @@
 import random
+import os
 from datetime import date
 import csv
 class BankAccount:
@@ -11,11 +12,9 @@ class BankAccount:
     while True:
       transaction_id =  random.randint(100000,999999)
       if transaction_id not in self.transactions:
-        self.transactions[transaction_id] = {"date": date.today().strftime("%d-%m-%Y"), "transaction": type, "amount" : amount}
+        self.transactions[transaction_id] = {"date": date.today().strftime("%d-%m-%Y"), "transaction": type, "amount" : amount, "balance": self.balance}
         break
-    with open("myaccount.csv","a",newline="") as file:
-      writer = csv.writer(file)
-      writer.writerow([transaction_id,self.transactions[transaction_id]["date"],type,amount])
+    self.update_csv(transaction_id,self.transactions[transaction_id]["date"], type, amount,self.balance)
 
   #Return the transactions
   def get_transactions_statement(self):
@@ -40,10 +39,44 @@ class BankAccount:
   def check_balance(self):
     return f"Current Balance: {self.balance}"
   
+  def update_csv(self,id,date,type,amount,balance):
+    write_header = not os.path.exists("myaccount.csv") or os.path.getsize("myaccount.csv") == 0
+    with open("myaccount.csv","a",newline="") as file:
+        writer = csv.writer(file)
+        if write_header:
+          writer.writerow(["Id","Date","Type","Amount","Balance"])
+        writer.writerow([id,date,type,amount,balance])
+
+  def load_from_previous_statements(self):
+    write_header = not os.path.exists("myaccount.csv") or os.path.getsize("myaccount.csv") == 0
+    if write_header:
+          return "Previous transactions not available"
+    
+    self.transactions = {}
+    self.balance = 0
+    with open("myaccount.csv","r") as file:
+      reader = csv.reader(file)
+      next(reader)
+      for row in reader:
+        if len(row) < 5:
+          continue
+        transaction_id = int(row[0])
+        tran_date = row[1]
+        tran_type = row[2]
+        tran_amount = int(row[3])
+        balance = int(row[4])
+
+        self.transactions[transaction_id] = {"date" : tran_date,
+          "transaction" : tran_type,
+          "amount" : tran_amount,
+          "balance" : balance}
+        self.balance = balance
+        
+
 
 my_account = BankAccount()
 while True:
-  operation = int(input("Please enter the transaction number \n 1. Deposit \n 2. Withdrawl \n 3. Statement \n 4. Check balance \n 5. Exit \n"))
+  operation = int(input("Please enter the transaction number \n 1. Deposit \n 2. Withdrawl \n 3. Statement \n 4. Check balance \n 5. Include previous transactions \n 6. Exit \n"))
   
   if operation == 1:
     amount = int(input("Please enter the deposit amount \n"))
@@ -55,19 +88,18 @@ while True:
 
   elif operation == 3:
     transactions = my_account.get_transactions_statement()
-    print("Transaction ID, Date, Type, Amount")
+    print("Transaction ID, Date, Type, Amount, Balance")
     for id, tran in transactions.items():
-      print(f"{id},{transactions[id]["date"]},{transactions[id]["transaction"]},{transactions[id]["amount"]}")
+      print(f'{id},{tran["date"]},{tran["transaction"]},{tran["amount"]},{tran["balance"]}')
 
   elif operation == 4:
     print(my_account.check_balance())
 
   elif operation == 5:
+    my_account.load_from_previous_statements()
+
+  elif operation == 6:
     break
 
   else:
     print("Please enter valid key: \n")
-
-
-
-  
