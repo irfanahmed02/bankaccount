3c5525bedd43240854e540064524348628eeecd9
irfanahmed02 <irfanahmed.s@yahoo.com>
2025-05-01 19:37:40 +0300

    refactor: change input keys

--- BankingUserInterface.py
+++ BankingUserInterface.py
@@ -3,12 +3,12 @@ import bankaccount
 user = bankaccount.UsersManagement()
 user.load_users_from_csv()
 while True:
-    user_input = input("Please select the below: \n 1. Create account \n 2. Sign in \n 3. Exit \n")
+    user_input = input("Please select the below: \n [C]reate account \n [S]ign in \n [E]xit \n").lower()
 
-    if user_input == "1":
+    if user_input == "c":
         print(user.create_account())
 
-    elif user_input == "2":
+    elif user_input == "s":
         username = user.signin()
         if not username:
             continue
@@ -17,40 +17,40 @@ while True:
             user_account.load_from_csv()
             while True:
 
-                operation = input("Please enter the transaction number \n 1. Deposit \n 2. Withdrawal \n 3. Statement \n 4. Check balance \n 5. Exit \n")
+                operation = input("Please enter the transaction number \n [D]eposit \n [W]ithdrawal \n [S]tatement \n Check [B]alance \n [E]xit \n").lower()
                 
-                if operation == "1":
+                if operation == "d":
                     try:
                         amount = float(input("Please enter the deposit amount \n"))
                         print(user_account.deposit(amount))
                     except ValueError:
                         print("Please enter the valid number")
 
-                elif operation == "2":
+                elif operation == "w":
                     try:
                         amount = float(input("Please enter withdraw amount \n"))
                         print(user_account.withdraw(amount))
                     except ValueError:
                         print("Please enter the valid number")
 
-                elif operation == "3":
+                elif operation == "s":
 
                     transactions = user_account.get_statement()
                     print("Transaction ID, Date, Type, Amount, Balance")
                     for id, tran in transactions.items():
                         print(f'{id},{tran["date"]},{tran["type"]},{tran["amount"]},{tran["balance"]}')
 
-                elif operation == "4":
+                elif operation == "b":
                     print(user_account.check_balance())
 
-                elif operation == "5":
+                elif operation == "e":
                     break
 
                 else:
                     print("Please enter valid key: \n")
 
-    elif user_input == "3":
+    elif user_input == "e":
         break
 
     else:
-        print("Please enter valid key")
+        print("Please enter valid key \n")
